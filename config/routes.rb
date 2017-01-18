62b112cd06713f4139331c1ff28cc0d9bbdfdecd
Siddhant Bajaj <sbajaj1997@gmail.com>
2017-01-18 14:51:53 +0530

    set root route to home

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'player#home'
 get '/player/odi/:id' => 'player#odi_info'
 get '/player/ipl/:id' => 'player#ipl_info'
 get '/player/test/:id' => 'player#test_info'
